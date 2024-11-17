b6b2b20fa33d79ff4284f402bd0debabbac3a8f9
Johny Lucio <johnylucio7@gmail.com>
2024-11-17 18:26:47 -0300

    [017] Creating an SDL Window

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game()
@@ -11,16 +12,46 @@ Game::~Game()
     std::cout << "Game destructor called!" << std::endl;
 }
 
-void Game::Initialize() {}
+void Game::Initialize()
+{
+    // Attempt to initialize all SDL subsystems
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "[Error | Class Game | Function 'Initialize()'] - Error initializing SDL!" << std::endl;
+        return;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS);
+
+    if (!window)
+    {
+        std::cerr << "[Error | Class Game | Function 'Initialize()'] - Error Creating SDL window!" << std::endl;
+        return;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer)
+    {
+        std::cerr << "[Error | Class Game | Function 'Initialize()'] - Error Creating SDL renderer!" << std::endl;
+        return;
+    }
+}
 
 void Game::Run()
 {
-    while (true)
-    {
-        ProcessInput();
-        Update();
-        Render();
-    }
+    // while (true)
+    // {
+    //     ProcessInput();
+    //     Update();
+    //     Render();
+    // }
 }
 
 void Game::ProcessInput() {}
