bb1493d4541ff3bde8459a16242ab33f0185116b
Johny Lucio <johnylucio7@gmail.com>
2024-11-18 10:33:02 -0300

    [027] SDL Delay

--- src/Game.cpp
+++ src/Game.cpp
@@ -129,8 +129,14 @@ void Game::Setup()
 void Game::Update()
 {
     // TODO: Update game objects
-    // TODO: If we are to fast, waste some time until we reach the MILLISECS_PER_FRAME
-    while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+
+    // If we are to fast, waste some time until we reach the MILLISECS_PER_FRAME
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+    
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME)
+    {
+        SDL_Delay(timeToWait);
+    }
 
     // Store the current frame time
     millisecsPreviousFrame = SDL_GetTicks();
