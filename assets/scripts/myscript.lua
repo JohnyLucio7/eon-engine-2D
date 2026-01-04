31ceef783c2f299a17e18b945a6e7cb877e6af5c
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2026-01-04 13:50:33 -0300

    [135] Lua State
    133. Game Scripting
    134. The Lua Scripting Language
    135. Lua State

---
+++ assets/scripts/myscript.lua
@@ -0,0 +1,9 @@
+-- This is a lua global variable
+some_variable = 7 * 6
+user_name = "Johny"
+
+print("Hello " .. user_name)
+
+if some_variable == 42 then
+    print("The value of the variable is " .. some_variable)
+end
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,24 @@
 #include "./Game/Game.h"
+#include <sol/sol.hpp>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
-    Game game;
+void TestLua() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    lua.script_file("./assets/scripts/myscript.lua");
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+    int someVariableInsideCpp = lua["some_variable"];
+    std::cout << "The value of some_variable in C++ is: " << someVariableInsideCpp << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    // Game game;
+    //
+    // game.Initialize();
+    // game.Run();
+    // game.Destroy();
+
+    TestLua();
 
     return 0;
 }
