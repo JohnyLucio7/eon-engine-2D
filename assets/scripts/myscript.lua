f6c0bace868123db321b6fcc46a77bfe8309c774
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2026-01-04 14:18:36 -0300

    [137] Lua Functions

--- assets/scripts/myscript.lua
+++ assets/scripts/myscript.lua
@@ -10,3 +10,13 @@ config = {
         height = 600
     }
 }
+
+-- This is a Lua function that calculates the factorial of a given number
+function factorial(n)
+    if n == 1 then
+        return 1
+    end
+    return n * factorial(n - 1)
+end
+
+print("The cube of the number 3 is equal to " .. cube(3))
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,9 +2,17 @@
 #include <sol/sol.hpp>
 #include <iostream>
 
+int nativeCppCubeFunction(int n) {
+    return (n * n * n);
+}
+
 void TestLua() {
     sol::state lua;
     lua.open_libraries(sol::lib::base);
+
+    // This is how we expose and bind a native C++ function to be used by the Lua script
+    lua["cube"] = nativeCppCubeFunction;
+
     lua.script_file("./assets/scripts/myscript.lua");
 
     int someVariableInsideCpp = lua["some_variable"];
@@ -17,6 +25,11 @@ void TestLua() {
 
     std::cout << "The value of isFullscreen is: " << isFullscreen << ", width: " << width << ", height: " << height <<
             std::endl;
+
+    // This is how we invoke a Lua function from inside C++
+    sol::function functionFactorial = lua["factorial"];
+    int functionResult = functionFactorial(5);
+    std::cout << "The Lua script calculated 5! as " << functionResult << std::endl;
 }
 
 int main(int argc, char *argv[]) {
